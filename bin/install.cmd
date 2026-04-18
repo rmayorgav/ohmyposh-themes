1206c72e9fecf245c7bec20c58da7ebeed761d19
Rodrigo Mayorga V. <rmayorgav@rmayorgav.com>
2026-04-17 22:45:54 -0400

    Update install.cmd

--- bin/install.cmd
+++ bin/install.cmd
@@ -1,3 +1,6 @@
-@mkdir %USERPROFILE%\.config
-@mkdir %USERPROFILE%\.config\oh-my-posh
-copy *.omp.json %USERPROFILE%\.config\oh-my-posh
+@echo off
+set ROOT_DIR=%~dp0\..
+echo %ROOT_DIR%
+mkdir %USERPROFILE%\.config
+mkdir %USERPROFILE%\.config\oh-my-posh
+copy %ROOT_DIR%\*.omp.json %USERPROFILE%\.config\oh-my-posh
